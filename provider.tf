5acf044d61334458730e3ed0e09af6335a4fa1d4
vitorrafaelm <vitor.rafael1518@gmail.com>
2024-05-02 23:02:59 -0300

    feat: add remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.47.0"
     }
+  }
+  backend "s3" {
+    bucket = "cominudadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
